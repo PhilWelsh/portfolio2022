b07b273e81c6b74e94d6f7359fb62d8794e9a7f3
unknown <phizs@hotmail.com>
2022-02-08 10:09:54 -0500

    remove strict mode temporarily

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   ignoreBuildErrors: true,
   images: {
     domains: ["image-component.nextjs.gallery", "res.cloudinary.com"],
